2fccd46e5eafad429629d5148fcde6cc847bbc5b
Lanny MacMillan <lannymacmillan@gmail.com>
2025-01-27 23:15:55 -0500

    testing github actions

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -3,6 +3,6 @@ import { greet } from '../src/index';
 describe('greet function', () => {
   it('should return a greeting message', () => {
     const result = greet('Node Forge');
-    expect(result).toBe('Hello, Node Forge!');
+    expect(result).toBe('Goodbye, Node Forge!');
   });
 });
