551250409858c273e7c1355bbbff0cca571cda83
Lanny MacMillan <lannymacmillan@gmail.com>
2025-01-30 15:28:21 -0500

    removed test to check action error message

--- tests/index.test.ts
+++
@@ -1,8 +0,0 @@
-// import { greet } from '../src/index';
-
-// describe('greet function', () => {
-//   it('should return a greeting message', () => {
-//     const result = greet('Node Forge');
-//     expect(result).toBe('Hello, Node Forge!');
-//   });
-// });
