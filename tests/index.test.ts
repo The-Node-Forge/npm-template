22b05c1ada768e4534ddb548fb3b0abb8a417f54
Lanny MacMillan <lannymacmillan@gmail.com>
2025-01-30 16:38:04 -0500

    copy change and added type to test

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -2,7 +2,7 @@ import { greet } from '../src/index';
 
 describe('greet function', () => {
   it('should return a greeting message', () => {
-    const result = greet('Node Forge');
-    expect(result).toBe('Hello, Node Forge!');
+    const result: string = greet('Node Forge Contributor');
+    expect(result).toBe('Hello, Node Forge Contributor!');
   });
 });
